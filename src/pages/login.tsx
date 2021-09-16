a5ea3437fcce57f4271f4880de99dd8975271180
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-09-17 00:02:34 +0900

    Login/Signupページ用意（内容は実装のタイミングで）

---
+++ src/pages/login.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import Layout from '@/components/Layout'
+
+const LoginPage: NextPage = () => {
+  return <Layout title='Log in'>Log in</Layout>
+}
+
+export default LoginPage
---
+++ src/pages/signup.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import Layout from '@/components/Layout'
+
+const SignupPage: NextPage = () => {
+  return <Layout title='Sign up'>Sign up</Layout>
+}
+
+export default SignupPage
