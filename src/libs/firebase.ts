9254a243dfd9cce39a8f66730a3dee42167b4806
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-09-28 22:33:24 +0900

    ignoreUndefinedPropertiesを有効化

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+import { initializeFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -14,4 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 export const firebaseAuth = getAuth(app)
-export const firestore = getFirestore(app)
+export const firestore = initializeFirestore(app, { ignoreUndefinedProperties: true })
