47fef42b33babbdaeb35cdd1c1fd096c72627c5d
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-09-30 00:10:35 +0900

    SetAvatarにアイコン表示

--- src/components/SetAvatar.tsx
+++ src/components/SetAvatar.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { IoCameraReverse } from 'react-icons/io5'
 import SetAvatarModal from '@/components/SetAvatarModal'
 import Avatar from '@/styles/styled-components/avatar.component'
 import Styles from '@/styles/set-avatar.style'
@@ -16,6 +17,9 @@ const SetAvatar: React.FC<Props> = ({ source }) => {
     <>
       <div css={Styles.root}>
         <Avatar css={Styles.image} src={source} onClick={() => setIsOpenModal(true)} />
+        <div css={Styles.editIconWrapper}>
+          <IoCameraReverse css={Styles.editIcon} />
+        </div>
       </div>
       {isOpenModal && <SetAvatarModal closeAction={() => setIsOpenModal(false)} />}
     </>
--- src/styles/set-avatar.style.ts
+++ src/styles/set-avatar.style.ts
@@ -1,6 +1,7 @@
 import { css } from '@emotion/react'
 
 const root = css`
+  position: relative;
   display: flex;
   justify-content: center;
 `
@@ -11,9 +12,29 @@ const image = css`
   cursor: pointer;
 `
 
+const editIconWrapper = css`
+  position: absolute;
+  bottom: 1rem;
+  left: calc(50% + 3.7rem);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 2.75rem;
+  height: 2.75rem;
+  border-radius: 50%;
+  color: var(--color-back);
+  background-color: var(--color-text);
+`
+
+const editIcon = css`
+  width: 1.5rem;
+`
+
 const styles = {
   root,
   image,
+  editIconWrapper,
+  editIcon,
 }
 
 export default styles
