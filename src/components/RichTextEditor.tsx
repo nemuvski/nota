371492a408418ebc7e8902ce9743a6f80e04a0cb
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-10-04 23:50:16 +0900

    RichEditor部分をスタイル調整

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -1,19 +1,40 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { Editor, EditorState, convertToRaw } from 'draft-js'
 import Styles from '@/styles/rich-text-editor.style'
+import Button from '@/styles/styled-components/button.component'
 
 const RichTextEditor = ({ ...props }) => {
   const [editorState, setEditorState] = useState(EditorState.createEmpty())
 
-  const handleEditorStateChange = (changedEditorState: EditorState) => {
-    setEditorState(changedEditorState)
-    return props.onChange(convertToRaw(changedEditorState.getCurrentContent()))
-  }
+  const handleEditorStateChange = useCallback(
+    (changedEditorState: EditorState) => {
+      setEditorState(changedEditorState)
+      // 変更内容を変換したものを渡す
+      return props.onChange(convertToRaw(changedEditorState.getCurrentContent()))
+    },
+    [props]
+  )
 
   return (
     <div css={Styles.root}>
-      <div css={Styles.actions}>ACTIONS</div>
-      <div css={Styles.editor}>
+      <ul css={Styles.actions}>
+        <li>
+          <Button type='button' color='gray'>
+            U
+          </Button>
+        </li>
+        <li>
+          <Button type='button' color='gray' title='bold'>
+            B
+          </Button>
+        </li>
+        <li>
+          <Button type='button' color='gray' title='italic'>
+            I
+          </Button>
+        </li>
+      </ul>
+      <div css={[Styles.editor, Styles.draftEditorStyles]}>
         <Editor editorState={editorState} onChange={handleEditorStateChange} />
       </div>
     </div>
--- src/styles/rich-text-editor.style.ts
+++ src/styles/rich-text-editor.style.ts
@@ -4,14 +4,36 @@ const root = css`
   width: 100%;
 `
 
-const actions = css``
+const actions = css`
+  margin: 0 0 0.75rem;
+  padding: 0;
+  list-style-type: none;
+`
 
-const editor = css``
+const editor = css`
+  overflow-y: auto;
+  width: 100%;
+  padding: 0.75rem;
+  border: 2px solid var(--color-gray-dark);
+  border-radius: 0.3rem;
+`
+
+// draft-jsで付与されるクラス
+const draftEditorStyles = css`
+  .DraftEditor-root {
+    width: 100%;
+    height: 12rem;
+    // フォントサイズは固定
+    font-size: 16px;
+    line-height: 1.6;
+  }
+`
 
 const styles = {
   root,
   actions,
   editor,
+  draftEditorStyles,
 }
 
 export default styles
