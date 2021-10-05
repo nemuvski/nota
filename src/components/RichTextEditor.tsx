586b605142fc6a35316f17565d9a3e03a09141b5
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-10-05 23:48:52 +0900

    RichTextEditorの装飾機能

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from 'react'
-import { Editor, EditorState, convertToRaw } from 'draft-js'
+import { Editor, EditorState, DraftEditorCommand, DraftHandleValue, convertToRaw, RichUtils } from 'draft-js'
 import Styles from '@/styles/rich-text-editor.style'
 import Button from '@/styles/styled-components/button.component'
 
@@ -10,32 +10,63 @@ const RichTextEditor = ({ ...props }) => {
     (changedEditorState: EditorState) => {
       setEditorState(changedEditorState)
       // 変更内容を変換したものを渡す
-      return props.onChange(convertToRaw(changedEditorState.getCurrentContent()))
+      props.onChange(convertToRaw(changedEditorState.getCurrentContent()))
     },
     [props]
+  )
+
+  // キーコマンドでの装飾のハンドラー
+  const handleKeyCommand = useCallback(
+    (command: DraftEditorCommand, changedEditorState: EditorState): DraftHandleValue => {
+      const newState = RichUtils.handleKeyCommand(changedEditorState, command)
+      if (newState) {
+        handleEditorStateChange(newState)
+        return 'handled'
+      }
+      return 'not-handled'
+    },
+    [handleEditorStateChange]
   )
 
   return (
     <div css={Styles.root}>
       <ul css={Styles.actions}>
         <li>
-          <Button type='button' color='gray'>
+          <Button
+            type='button'
+            color='gray'
+            onClick={() => {
+              handleEditorStateChange(RichUtils.toggleInlineStyle(editorState, 'UNDERLINE'))
+            }}
+          >
             U
           </Button>
         </li>
         <li>
-          <Button type='button' color='gray' title='bold'>
+          <Button
+            type='button'
+            color='gray'
+            onClick={() => {
+              handleEditorStateChange(RichUtils.toggleInlineStyle(editorState, 'BOLD'))
+            }}
+          >
             B
           </Button>
         </li>
         <li>
-          <Button type='button' color='gray' title='italic'>
+          <Button
+            type='button'
+            color='gray'
+            onClick={() => {
+              handleEditorStateChange(RichUtils.toggleInlineStyle(editorState, 'ITALIC'))
+            }}
+          >
             I
           </Button>
         </li>
       </ul>
       <div css={[Styles.editor, Styles.draftEditorStyles]}>
-        <Editor editorState={editorState} onChange={handleEditorStateChange} />
+        <Editor editorState={editorState} onChange={handleEditorStateChange} handleKeyCommand={handleKeyCommand} />
       </div>
     </div>
   )
