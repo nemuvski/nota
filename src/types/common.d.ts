c8d3df9eca405c3561f4f00230ae0a8242540827
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-10-06 18:03:09 +0900

    カスタムの型定義を分ける

--- src/types/common.d.ts
+++ src/types/common.d.ts
@@ -7,13 +7,3 @@ declare type Level = 'success' | 'info' | 'warning' | 'error'
  * 色
  */
 declare type Color = 'primary' | 'secondary' | 'gray'
-
-/**
- * Firebase Authenticationで発行されるUIDのエイリアス
- */
-declare type AuthUid = string
-
-/**
- * Firestoreで発行されるドキュメントのIDのエイリアス
- */
-declare type FirestoreDocumentId = string
---
+++ src/types/firebase.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Firebase Authenticationで発行されるUIDのエイリアス
+ */
+declare type AuthUid = string
+
+/**
+ * Firestoreで発行されるドキュメントのIDのエイリアス
+ */
+declare type FirestoreDocumentId = string
