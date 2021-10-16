5122bb1afbff352a9745b9c819e9e31fb9445bfb
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-10-17 00:06:05 +0900

    削除ボタン押下時にモーダル表示

--- src/pages/own/articles/[docId].tsx
+++ src/pages/own/articles/[docId].tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
 import { useRouter } from 'next/router'
 import { useMyArticle } from '@/hooks/article'
 import Layout from '@/components/Layout'
 import Loading from '@/components/Loading'
 import NotFoundContent from '@/components/NotFoundContent'
 import ArticleDetail from '@/components/ArticleDetail'
+import Modal from '@/components/Modal'
 import Styles from '@/styles/own-article-detail-page.style'
+import ButtonList from '@/styles/styled-components/button-list.component'
 import Button from '@/styles/styled-components/button.component'
+import Paragraph from '@/styles/styled-components/paragraph.component'
 
 type QueryParams = {
   docId: FirestoreDocumentId
@@ -15,6 +19,8 @@ type QueryParams = {
 const OwnArticleDetailPage: NextPage = () => {
   const router = useRouter()
   const { docId } = router.query as QueryParams
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   const { isFetching, article } = useMyArticle(docId)
 
@@ -41,10 +47,24 @@ const OwnArticleDetailPage: NextPage = () => {
         >
           Edit
         </Button>
-        <Button type='button' color='secondary'>
+        <Button type='button' color='secondary' onClick={() => setIsModalOpen(true)}>
           Delete
         </Button>
       </div>
+
+      {isModalOpen && (
+        <Modal closeAction={() => setIsModalOpen(false)}>
+          <Paragraph alignment='center'>Are you sure to delete this article?</Paragraph>
+          <ButtonList alignment='center'>
+            <Button type='button' color='gray' onClick={() => setIsModalOpen(false)}>
+              Cancel
+            </Button>
+            <Button type='button' color='secondary'>
+              Delete
+            </Button>
+          </ButtonList>
+        </Modal>
+      )}
     </Layout>
   )
 }
